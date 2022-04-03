b5d017b4ec6952c397feaeddddf9643b9b78e693
Kleszcz1337 <mijoj.koworow@gmail.com>
2022-04-03 20:39:21 +0200

    Class4 fully done

--- Class4/Task3.cpp
+++ Class4/Task3.cpp
@@ -25,8 +25,85 @@ int main() {
 size_t length(const char* cstr){
     int counter = 0;
 
-    while(cstr[counter] != '/0')
+    while(cstr[counter] != '\0')
         counter++;
 
     return counter;
+}
+
+bool   isLetter(char c){
+    return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z');
+}
+
+char*  reverse(char* cstr){
+    size_t size = length(cstr)-1;
+
+    char tmp;
+    for(size_t i = 0; i < size/2; i++){
+        tmp = cstr[i];
+        cstr[i] = cstr[size-i];
+        cstr[size-i] = tmp;
+    }
+    return cstr;
+}
+
+size_t words(const char* cstr){
+    size_t size = length(cstr);
+    size_t result = 0;
+    size_t index = 0;
+
+    while (index < size) {
+        if (!isLetter(cstr[index]) && index + 1 < size) {
+            index++;
+            if (isLetter(cstr[index])) {
+                result++;
+                while (isLetter(cstr[index]) && index < size) {
+                    index++;
+                }
+            }
+        }
+        else {
+            index++;
+        }
+    }
+    return result;
+}
+
+size_t words2(const char* cstr) {
+    size_t size = length(cstr);
+    size_t result = 0;
+
+    size_t index = 0;
+    while (index < size) {
+        if (!isLetter(cstr[index]) && index + 1 < size) {
+            index++;
+            if (isLetter(cstr[index])) {
+                size_t counter = 0;
+                while (isLetter(cstr[index]) && index < size) {
+                    index++;
+                    counter++;
+                }
+                if (counter >= 2) {
+                    result++;
+                }
+            }
+        }
+        else {
+            index++;
+        }
+    }
+    return result;
+}
+
+char*  concat(char* t, const char* s){
+    size_t sizeT = length(t);
+    size_t sizeS = length(s);
+
+    for(size_t i = 0; i < sizeS; i++){
+        t[sizeT] = s[i];
+        sizeT++;
+    }
+    t[sizeT+sizeS+1] = '\0';
+
+    return t;
 }
